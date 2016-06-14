12ebc6c0aff6196b2f6caf69f6a21aaf48cb0a2f
boyxp <isme@live.com>
2016-06-14 22:37:30 +0800

    新增renderer构件html实现

---
+++ src/renderer/adapter/html.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+namespace bluefin\pattern\renderer\adapter;
+use bluefin\pattern\renderer\renderer as rendererInterface;
+class html extends \injector implements rendererInterface
+{
+	private $_tplPath = '';
+
+	public function __construct($tplPath)
+	{
+		$this->_tplPath = $tplPath;
+	}
+
+	public function render($content):bool
+	{
+		if(strpos($this->_tplPath, '%s')) {
+			$handle = static::$locator->router->getHandle();
+			if(is_array($handle)) {
+				$handle = implode('\\', $handle);
+			}
+
+			$handle  = strtr($handle, ['::'=>'\\', '\\'=>'/']);
+			$handle  = substr($handle, strpos($handle, '/')+1);
+			$tplPath = sprintf($this->_tplPath, $handle);
+		} else {
+			$tplPath = $this->_tplPath;
+		}
+
+		if(is_file($tplPath)) {
+			extract($content);
+			include($tplPath);
+			return true;
+		}
+
+		return false;
+	}
+}
